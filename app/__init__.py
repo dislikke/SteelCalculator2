69d3c0de972aaac1ea475917421de7c82d24b3d3
Diana <Diana.fedotova2002@mail.ru>
2025-12-21 22:16:40 +0500

    Fix prometheus metrics init

--- app/__init__.py
+++ app/__init__.py
@@ -32,6 +32,6 @@ def create_app(config_class=Config):
     #    db.create_all()
     metrics = PrometheusMetrics(app)
     metrics.info("app_info", "SteelCalculator application info", version="1.0.0")
-    metrics.defaults()
+
 
     return app
