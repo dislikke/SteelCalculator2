9639abc7a8297c6f5bcd4221a6ad15a397bbda27
Diana <Diana.fedotova2002@mail.ru>
2025-12-21 20:21:27 +0500

    21.12.2025 25

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from .routes.variants import variants_bp
 from .routes.plots import plots_bp
 
 from .routes.report import report_bp
-
+from prometheus_flask_exporter import PrometheusMetrics
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -29,5 +29,13 @@ def create_app(config_class=Config):
 
     # with app.app_context():
     #    db.create_all()
+    metrics = PrometheusMetrics(app)
+    metrics.info(
+        "app_info",
+        "SteelCalculator application info",
+        version="1.0.0"
+    )
+
+
 
     return app
